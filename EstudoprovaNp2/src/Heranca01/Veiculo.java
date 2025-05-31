1125ef3e528a05d7236077fcce59d4d471718230
Clovin <fudeoagr12@gmail.com>
2025-05-31 17:29:12 -0300

    de volta ao zero

--- EstudoprovaNp2/src/Heranca01/Veiculo.java
+++
@@ -1,5 +0,0 @@
-package Heranca01;
-
-public class Veiculo {
-
-}
--- TesteTeclado/src/SomaDeNumeros.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class SomaDeNumeros {
-
-    Scanner sc = new Scanner(System.in);
-    double numero = sc.nextDouble();
-    double
-
-}
