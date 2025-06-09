9f8b63d606db448033bd3401e23d0d40c8e8b0de
Clovin <fudeoagr12@gmail.com>
2025-06-09 15:11:24 -0300

    repositorio

---
+++ Vecto/src/ExerciciosFixacacao/Program.java
@@ -0,0 +1,14 @@
+package ExerciciosFixacacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Rend[] vecto = new Rend[10];
+
+
+    }
+}
---
+++ Vecto/src/ExerciciosFixacacao/Rend.java
@@ -0,0 +1,31 @@
+package ExerciciosFixacacao;
+
+public class Rend {
+    private String nome;
+    private String gmail;
+
+    public Rend(){
+
+    }
+    public Rend(String nome, String gmail){
+        this.nome = nome;
+        this.gmail = gmail;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+}
