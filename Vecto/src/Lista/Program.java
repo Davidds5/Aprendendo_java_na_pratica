d48c2b7d22381d58779fca00dc9c988d13373da1
Clovin <fudeoagr12@gmail.com>
2025-06-14 12:16:21 -0300

    AtividadeFixacao

--- Vecto/src/Lista/Program.java
+++ Vecto/src/Lista/Program.java
@@ -2,6 +2,7 @@ package Lista;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,19 +19,32 @@ public class Program {
 
         System.out.println(list.size());
         System.out.println("-----------------------------------------------");
-        for (String nomes : list){
+        for (String nomes : list) {
             System.out.println(nomes);
 
         }
         System.out.println("________________________________________________");
         list.remove(1);
-        list.removeIf(x -> x.charAt(0) == 'M');
+        list.removeIf(x -> x.charAt(0) == 'M'); // Remover todos os itens com a primeira letra M
         for (String nomes : list) {
             System.out.println(nomes);
         }
         System.out.println("________________________________________________");
+        // pocurando os nomes na list
         System.out.println("Index Of Alex: " + list.indexOf("Alex"));
         System.out.println("Index OF Bob: " + list.indexOf("Bob"));
 
+        System.out.println("________________________________________________");
+        // imprimir todos os nomes que comecam com a letra A
+        List<String> resultado = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String nomes : resultado) {
+            System.out.println(nomes);
+        }
+        System.out.println("-------------------------------------------------");
+        //  Primeiro elemento que comece com a letra A
+        // Se nao tiver o elemento ele volta null
+
+        String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
+        System.out.println(name);
     }
 }
