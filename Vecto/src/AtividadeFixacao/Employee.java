5d586e60d1e2de89ae2956add2ebc4e346f72c83
Clovin <fudeoagr12@gmail.com>
2025-06-15 10:36:03 -0300

    apaga

--- Vecto/src/AtividadeFixacao/Employee.java
+++ Vecto/src/AtividadeFixacao/Employee.java
@@ -1,40 +1,15 @@
 package AtividadeFixacao;
 
 public class Employee {
-    private Integer id;
     private String name;
+    private Integer id;
     private Double salary;
 
-    public Employee(Integer id, String name, Double salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    // construtor sem argumnetos
+    public Employee(){
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-    public void increaseSalary(double percentage){
-        salary += salary * percentage/ 100;
+    public Employee(String name, Integer id, Double salary){
+        
     }
 }
