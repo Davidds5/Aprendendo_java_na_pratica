c0e91962b55b76b4d127ef39408f72b4f4c1547d
Clovin <fudeoagr12@gmail.com>
2025-06-25 00:27:27 -0300

    Reforcando a Logica

---
+++ ProjetoReforcoLogica/src/ReforcoLogicaIfElse/At04.java
@@ -0,0 +1,53 @@
+package ReforcoLogicaIfElse;
+
+
+import java.util.Scanner;
+
+public class At04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo a calculadora do David");
+        System.out.println("_______________________________________________");
+        System.out.println("ESCOLHA AS OPERACOES!!!");
+        System.out.println("_______________________________________________");
+        System.out.println("1 - adicao");
+        System.out.println("2 - subtracao");
+        System.out.println("3 - multiplicacao");
+        System.out.println("4 - divisao");
+        System.out.println("5 - sair");
+        int opcao = sc.nextInt();
+
+        if (opcao >=  1 && opcao  <=4 ){
+            System.out.println("Informe o primeiro numero: ");
+            int number1 = sc.nextInt();
+            System.out.println("Informe o segundo numero: ");
+            int number2 = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Resultado : " + (number1 + number2));
+                    break;
+                case 2:
+                    System.out.println("Resultado : " + (number1 - number2));
+                    break;
+                case 3:
+                    System.out.println("Resultado : " + (number1 * number2));
+                    break;
+                case 4:
+                    if (number2 != 0) {
+                        System.out.println("Resultado : " + (number1 / number2));
+                    }else {
+                        System.out.println("Divisao por zero invalida");
+                    }
+                    break;
+
+            }
+        } else if (opcao == 5) {
+            System.out.println("Programa encerrando........");
+
+        }else {
+            System.out.println("Opcao Invalida!!");
+        }
+
+    }
+}
