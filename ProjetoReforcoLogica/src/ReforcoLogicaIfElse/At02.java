a4668056cd39d218e4c6abe9dec5bdd330fb8743
Clovin <fudeoagr12@gmail.com>
2025-06-24 23:56:54 -0300

    AulaReforcoDelogica

--- ProjetoReforcoLogica/src/ReforcoLogicaIfElse/At02.java
+++ ProjetoReforcoLogica/src/ReforcoLogicaIfElse/At02.java
@@ -5,5 +5,51 @@ import java.util.Scanner;
 public class At02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double n1 = sc.nextDouble();
+        System.out.println("Digite a segunda nota: ");
+        double n2 = sc.nextDouble();
+
+        double media = (n1 + n2) / 2;
+
+        if(media > 7){
+            System.out.println("Parabens voce foi aprovado !!!!");
+
+        }else {
+            System.out.println("Infelizmente voce ficou de exame: ");
+            System.out.println("Media atual: " + media);
+            System.out.println("Informe a nota do exame: ");
+            double notaExame = sc.nextDouble();
+
+            double mediaExame = (n1 + n2 + notaExame) / 3;
+
+
+            if (mediaExame >= 6){
+                System.out.println("Parabens voce foi aprovado !!!");
+                System.out.println("Media atual : " + mediaExame);
+
+            }else if ( mediaExame >= 4){
+                System.out.println("Ficou de DP, media apos Exame: " + mediaExame);
+                System.out.println("Informe a nota da DP: ");
+                double Dpnota = sc.nextDouble();
+
+                double MediaDp = (n1 + n2 + mediaExame + Dpnota) / 4;
+
+                if (MediaDp >= 6){
+                    System.out.println("Parabens voce passou, media atual :" + MediaDp);
+
+                }else {
+                    System.out.println("Infelizmente reprovado, mesmo com a DP");
+                }
+
+            }else{
+                System.out.println("Infelizmente reprovado apos o exame com media : " + mediaExame);
+
+            }
+        }
+
+
+
     }
-}
+    }
---
+++ ProjetoReforcoLogica/src/ReforcoLogicaIfElse/At03.java
@@ -0,0 +1,37 @@
+package ReforcoLogicaIfElse;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class At03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> listNUmber = new ArrayList<>();
+
+        System.out.println("Quantos numeros deseja digitar: ");
+        int nDigitados = sc.nextInt();
+
+        System.out.println("Digite os numeros: ");
+        for (int i = 0; i <nDigitados ; i++) {
+            System.out.println("Digite o numero na posicao " + (i + 1) + ":");
+            int n = sc.nextInt();
+
+            listNUmber.add(n);
+        }
+
+        int maiorNumber = listNUmber.get(0);
+
+        for (int i = 0; i <listNUmber.size() ; i++) {
+            if (maiorNumber < listNUmber.get(i)){
+                maiorNumber = listNUmber.get(i);
+                System.out.println("Maior Numero: " + maiorNumber);
+            }
+
+        }
+
+    }
+}
+
