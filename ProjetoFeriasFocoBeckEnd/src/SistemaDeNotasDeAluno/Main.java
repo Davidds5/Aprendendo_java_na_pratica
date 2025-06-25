b1b23c8498d31d93071cb077cb25b3e0f3d7f9db
Clovin <fudeoagr12@gmail.com>
2025-06-25 19:46:21 -0300

    Termino do projeto de lista de Alunos

--- ProjetoFeriasFocoBeckEnd/src/SistemaDeNotasDeAluno/Main.java
+++ ProjetoFeriasFocoBeckEnd/src/SistemaDeNotasDeAluno/Main.java
@@ -15,6 +15,8 @@ public class Main {
             System.out.println("3 - BUSCA ALUNOS POR MATRICULA");
             System.out.println("4 - MOSTRA RESULTADO");
             System.out.println("0 - SAIR");
+
+            System.out.println("Digite sua opcao: ");
             opcao = sc.nextInt();
             sc.nextLine();
 
@@ -36,12 +38,47 @@ public class Main {
                     }
                     sc.nextLine();
 
-                    Aluno novoAluno = new Aluno("David", "GR591", notas);
+                    Aluno novoAluno = new Aluno(nome, matricula, notas);
                     turma.adicionarAlunos(novoAluno);
                     System.out.println("Novo aluno adiconado com sucesso");
                     break;
+
+
+                case 2:
+                    System.out.println("=== LISTA DE ALUNOS ===");
+                    turma.listarAlunos();
+                    break;
+
+
+                case 3:
+                    System.out.println("Informe a matricula do aluno");
+                    String Buscamatricula = sc.nextLine();
+                    Aluno alunoEcontrado = turma.buscaMatricula(Buscamatricula);
+                    if (alunoEcontrado != null){
+                        System.out.println(alunoEcontrado);
+                    }
+                    else {
+                        System.out.println("Aluno nao encontrado");
+
+                    }
+                    break;
+                case 4:
+                    turma.mostraResulado();
+                    break;
+
+                case 0:
+                    System.out.println("Saindo ......");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida.");
+
+
+
             }
-        }
+        }while(opcao != 0);
+
+        sc.close();
 
     }
 }
